3582f1764cec01f5a90adfbca04ff876a32bb8b6
Tobias laptop <tralsted@hotmail.dk>
2026-05-09 19:30:59 +0200

    first lab done

---
+++ Core/src/main/java/dk/sdu/cbse/App.java
@@ -0,0 +1,10 @@
+package dk.sdu.cbse;
+
+ import javafx.application.Application;
+ import javafx.stage.Stage;
+
+ public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
